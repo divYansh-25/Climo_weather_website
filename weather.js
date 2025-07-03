e2153fb6e0841c45feb7eec54667ab3e11a2ae1b
divYansh-25 <devu0425@gmail.com>
2025-07-03 16:48:50 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,7 +2,6 @@ let city = document.getElementById("city");
 let country = document.getElementById("country");
 let temp = document.getElementById("temp");
 let images = document.getElementById("img");
-// let input = document.getElementById("input").value;
 let type = document.getElementById("type");
 let pressure = document.getElementById("pressure");
 let humidity = document.getElementById("humidity");
@@ -12,57 +11,75 @@ let ht2 = document.getElementById("hour2temp");
 let ht3 = document.getElementById("hour3temp");
 let ht4 = document.getElementById("hour4temp");
 let c1 = document.getElementById("c1t");
-let c2 = document.getElementById("c2t"); 
+let c2 = document.getElementById("c2t");
 let c3 = document.getElementById("c3t");
 let c4 = document.getElementById("c4t");
+let input = document.getElementById("input");
 
-let API_KEY = "a9c8bbab47d7a752c0963453fab539ca";
+let day = document.getElementById("day");
+let date = document.getElementById("date");
+let time = document.getElementById("tim");
 
-const getWeather = async function(search) {
+const unsplashKey = "3MJl4ZcCS2WuZJbYi808v20W5qypjkQ30vV6Z2Zft8M"; // ⬅️ Replace with your actual Unsplash API key
+const API_KEY = "a9c8bbab47d7a752c0963453fab539ca";
+
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000); // visible for 3 seconds
+}
+
+
+
+
+
+
+// Event listener to detect Enter key in input
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    myFun();
+  }
+});
+
+// Unsplash background function
+function getCityImage(search) {
+  fetch(`https://api.unsplash.com/search/photos?query=${search}&client_id=${unsplashKey}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.results && data.results.length > 0) {
+        const imageUrl = data.results[0].urls.regular;
+        const timeBox = document.querySelector(".time");
+        timeBox.style.backgroundImage = `url(${imageUrl})`;
+        timeBox.style.backgroundSize = "cover";
+        timeBox.style.backgroundPosition = "center";
+        day.style.color = "white";
+        date.style.color = "white";
+      } else {
+        url("images/cbg.jpg");
+        timeBox.style.backgroundImage = url("images/cbg.jpg");
+        day.style.color = "black";
+        date.style.color = "black";
+      }
+    })
+    .catch(err => console.error("Image fetch error:", err));
+}
+
+// Main weather function
+const getWeather = async function (search) {
   let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}&units=metric`
   );
-  console.log(response);
 
   let data = await response.json();
-  console.log(data);
 
-
-  let latt = data.coord.lat;
-  // console.log(latt);
-  let lonn = data.coord.lon;
-  // console.log(lonn);
-
-  // const getforecast = async function() {
-  //   let respon = await fetch(`https://api.openweathermap.org/data/2.5/forecast/hourly?lat=${latt}&lon=${lonn}&appid=${API_KEY}`);
-  //   console.log(respon);
-  
-  //   let da = await respon.json();
-  //   console.log(da);
-  
-  //   // ht1.innerHTML = da.list[0].main.temp + "°C";
-  //   // ht2.innerHTML = da.list[1].main.temp + "°C";
-  //   // ht3.innerHTML = da.list[2].main.temp + "°C";
-  //   // ht4.innerHTML = da.list[3].main.temp + "°C";
-  
-  //   if(da.cod == "400") {
-  //     ht1.innerHTML ="--";
-  //     ht2.innerHTML ="--";
-  //     ht3.innerHTML ="--";
-  //     ht4.innerHTML ="--";
-  //   }
-  //   if(da.cod == "404") {
-  //     ht1.innerHTML ="--";
-  //     ht2.innerHTML ="--";
-  //     ht3.innerHTML ="--";
-  //     ht4.innerHTML ="--";
-  //   }
-  // }
-  // getforecast();
-
-
-  if(data.cod == "400") {
-    alert("Please enter a valid city name");
+  if (data.cod == "400" || data.cod == "404" || data.cod == "401") {
+    showToast("City Not Found");
     city.innerHTML = "City not found";
     country.innerHTML = "";
     temp.innerHTML = "";
@@ -70,102 +87,114 @@ const getWeather = async function(search) {
     humidity.innerHTML = "Humidity : --";
     wind.innerHTML = "Wind Speed : --";
     pressure.innerHTML = "Pressure : --";
+    return;
   }
-  if(data.cod == "404") {
-    alert("City not found");
-    city.innerHTML = "City not found";
-    country.innerHTML = "";
-    temp.innerHTML = "";
-    type.innerHTML = "";
-    humidity.innerHTML = "Humidity : --";
-    wind.innerHTML = "Wind Speed : --";
-    pressure.innerHTML = "Pressure : --";
-  }
-  if(data.cod == "401") {
-    alert("City not found");
-    city.innerHTML = "City not found";
-    country.innerHTML = "";
-    temp.innerHTML = "";
-    type.innerHTML = "";
-    humidity.innerHTML = "Humidity : --";
-    wind.innerHTML = "Wind Speed : --";
-    pressure.innerHTML = "Pressure : --";
-  }
-  if(search == ""){
-    alert("Please Enter Valid City Name :)")
-  }
-
 
   city.innerHTML = data.name;
   temp.innerHTML = data.main.temp + "°C";
-  tt=data.sys.country;
-  if(tt == "IN"){
-    country.innerHTML = "India";
-  }else{
-    country.innerHTML = data.sys.country;
+  let tt = data.sys.country;
+  country.innerHTML = tt == "IN" ? "India" : tt;
+  type.innerHTML = data.weather[0].main;
+
+
+
+
+
+
+  const iconCode = data.weather[0].icon;
+  images.src = `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+  if(type.innerHTML=="Clear"){
+    images.src = "images/sunny.png";
   }
 
-  // type.innerHTML = data.weather[0].description;
-  type.innerHTML = data.weather[0].main;
   humidity.innerHTML = "Humidity : " + data.main.humidity + " %";
   wind.innerHTML = "Wind Speed : " + data.wind.speed + " m/s";
   pressure.innerHTML = "Pressure : " + data.main.pressure + " hPa";
 
-  if(type.innerHTML == "Clouds"){
-    images.src="images/cloudy.jpg";
-  }else if(type.innerHTML == "Rain"){
-    images.src="images/rainy.jpg";
-  }else if(type.innerHTML == "Clear"){
-    images.src="images/sunny.png";}
-  else if(type.innerHTML == "Mist"){
-    images.src="images/mist.jpg";}
-  else if(type.innerHTML == "Snow"){
-    images.src="images/snowy.jpg";}
-  else if(type.innerHTML == "Thunderstorm"){
-    images.src="images/lightning.jpg";
-  }else if(type.innerHTML == "Drizzle"){
-    images.src="images/drizzle.jpg";}
-  else if(type.innerHTML == "Smoke"){
-    images.src="images/smoke.jpg";}
-  else if(type.innerHTML == "Dust"){
-    images.src="images/dust.jpg";}
-    else if(type.innerHTML == "Fog"){
-    images.src="images/foggy.jpg";}
-  else if(type.innerHTML == "Sand"){
-    images.src="images/sand.jpg";}
-  else if(type.innerHTML == "Squall"){
-    images.src="images/squall.jpg";}
-  else if(type.innerHTML == "Tornado"){
-    images.src="images/tornado.jpg";
-  }   
+  // switch (type.innerHTML) {
+  //   case "Clouds": images.src = "images/cloudy.jpg"; break;
+  //   case "Rain": images.src = "images/rainy.jpg"; break;
+  //   case "Clear": images.src = "images/sunny.png"; break;
+  //   case "Mist": images.src = "images/mist.jpg"; break;
+  //   case "Snow": images.src = "images/snowy.jpg"; break;
+  //   case "Thunderstorm": images.src = "images/lightning.jpg"; break;
+  //   case "Drizzle": images.src = "images/drizzle.jpg"; break;
+  //   case "Smoke": images.src = "images/smoke.jpg"; break;
+  //   case "Dust": images.src = "images/dust.jpg"; break;
+  //   case "Fog": images.src = "images/foggy.jpg"; break;
+  //   case "Sand": images.src = "images/sand.jpg"; break;
+  //   case "Squall": images.src = "images/squall.jpg"; break;
+  //   case "Tornado": images.src = "images/tornado.jpg"; break;
+  // }
 
   function formatTime(timestamp) {
     const date = new Date(timestamp * 1000);
     return date.toLocaleTimeString();
   }
 
-
-
   c1.innerHTML = formatTime(data.sys.sunrise);
   c2.innerHTML = formatTime(data.sys.sunset);
-  c3.innerHTML = data.visibility/1000 + " km";
+  c3.innerHTML = data.visibility / 1000 + " km";
 }
 
-function myFun(){
-    search=input.value;
-    getWeather(search);
+// Search city on button click or Enter
+function myFun() {
+  let search = input.value.trim();
+  if (search === "") {
+    showToast("Please Enter a valid City Name");
+
+
+    return;
+  }
+  getWeather(search);
+  getCityImage(search);
 }
 
-function myFun1(){
-    search="ghaziabad";
-    getWeather(search);
+// Geolocation search
+function myFun1() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  } else {
+    // alert("Geolocation is not supported by this browser.");
+    showToast("Geolocation is not supported by this browser.");
+
+  }
+
+  function successCallback(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.length > 0) {
+          const cityName = data[0].name;
+          getWeather(cityName);
+          getCityImage(cityName);
+        } else {
+          
+          showToast("Unable to determine your location.");
+        }
+      })
+      .catch(() => {
+        showToast("Failed to fetch location data.");
+      });
+  }
+
+  function errorCallback(error) {
+    showToast("Location access denied or unavailable.");
+    console.error(error);
+  }
 }
 
-
-let day = document.getElementById("day");
-let date = document.getElementById("date");
-let time = document.getElementById("tim");
+// Time & date setup 
 let d = new Date();
 time.innerHTML = d.toLocaleString("default", { hour: "2-digit", minute: "2-digit" });
 day.innerHTML = d.toLocaleString("default", { weekday: "long" });
-date.innerHTML = d.toLocaleString("default", { month: "long", day: "numeric",year: "numeric" });
+date.innerHTML = d.toLocaleString("default", { month: "long", day: "numeric", year: "numeric" });
+
+window.onload = () => {
+  // input.focus();
+};
+
+
